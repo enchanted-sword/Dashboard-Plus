083ce381bf138ed57ece358d0717691595e4f683
enchanted-sword <xlucas4956@gmail.com>
2024-01-13 13:47:12 -0400

    messaging scale

---
+++ source/scripts/messagingScale.js
@@ -0,0 +1,32 @@
+import { getPreferences } from './utility/jsTools.js';
+import { style, s } from './utility/style.js';
+
+const styleElement = style('');
+
+const run = ({ scale }) => {
+  if (scale.enabled === false) return false;
+  
+  styleElement.innerText = `
+    [data-css~="conversationWindow"] {
+      --dbplus-messagingScale: ${scale.value} !important;
+
+      &:has([data-css~="conversation"]:not(.dbplus-revertMessaging)) {
+        width: calc(360px * var(--dbplus-messagingScale)); 
+        height: calc(560px * var(--dbplus-messagingScale));
+        max-height: calc(100vh - 80px);
+      }
+    }
+  `;
+
+  return true;
+};
+
+export const main = async () => {
+  const preferences = await getPreferences('messagingScale');
+
+  if (run(preferences)) document.head.append(styleElement);
+};
+
+export const clean = async () => styleElement.remove();
+
+export const update = async ({ preferences }) => run(preferences);
