0f1fd4c2a53a631abc0627257ff71fc181a8f95e
enchanted-sword <xlucas4956@gmail.com>
2024-02-10 21:15:51 -0400

    apply to blog selector and tab management

--- source/scripts/cornerRadii.js
+++ source/scripts/cornerRadii.js
@@ -9,7 +9,8 @@ const run = ({ value }) => {
   styleElement.innerText = `
     ${s('postColumn')} > ${s('bar')}, 
     ${s('activityPopover')} ${s('selectorPopover')},
-    article${s('post')} { border-radius: ${value.value}px !important; }
+    article${s('post')}, ${s('tabManagement')},
+    ${s('selectorPopover')}:has(${s('blogsList')}) { border-radius: ${value.value}px !important; }
     article${s('post')} header { border-radius: ${value.value}px ${value.value}px 0 0 !important; }
   `;
 
