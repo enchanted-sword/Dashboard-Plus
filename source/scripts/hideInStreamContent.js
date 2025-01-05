84fe6ceec0bc1740d5e20104b89c869258537365
enchanted-sword <xlucas4956@gmail.com>
2025-01-04 21:37:18 -0400

    wawa

--- source/scripts/hideInStreamContent.js
+++ source/scripts/hideInStreamContent.js
@@ -2,11 +2,12 @@ import { getOptions } from './utility/jsTools.js';
 import { style, s } from './utility/style.js';
 import { mutationManager } from './utility/mutations.js';
 
-let blogRecommendations, tagRecommendations, blazedContent, inStreamAds;
+let blogRecommendations, tagRecommendations, blazedContent, inStreamAds, communities;
 const blogRecSelector = `[data-cell-id*="timelineObject:carousel"] ${s('listTimelineObject')}:has(${s('blogLinkRecommendationWrapper')})`;
 const tagRecSelector = `[data-cell-id*="timelineObject:carousel"] ${s('listTimelineObject')}:has(${s('tagCard')})`;
 const blazedSelector = `${s('listTimelineObject')}:has([href="#managed-icon__badge-blaze"])`;
 const adSelector = `${s('listTimelineObject')}:has(${s('adTimelineObject')})`;
+const communitiesRecSelector = `[data-cell-id*="timelineObject:carousel"] ${s('listTimelineObject')}:has(${s('communityCard')})`;
 const hiddenClass = 'dbplus-hiddenInStream';
 const styleElement = style(`
   [data-cell-id*="timelineObject:title"]:has(+ [data-cell-id*="timelineObject:carousel"] ${s('listTimelineObject')}.${hiddenClass}),
@@ -17,12 +18,13 @@ const hideInStreamElements = elements => $(elements).addClass(hiddenClass);
 
 export const main = async () => {
   const selectors = [];
-  ({ blogRecommendations, tagRecommendations, blazedContent, inStreamAds } = await getOptions('hideInStreamContent'));
+  ({ blogRecommendations, tagRecommendations, blazedContent, inStreamAds, communities } = await getOptions('hideInStreamContent'));
 
   if (blogRecommendations) selectors.push(blogRecSelector);
   if (tagRecommendations) selectors.push(tagRecSelector);
   if (blazedContent) selectors.push(blazedSelector);
   if (inStreamAds) selectors.push(adSelector);
+  if (communities) selectors.push(communitiesRecSelector);
 
   if (selectors.length) {
     mutationManager.start(selectors.join(','), hideInStreamElements);
