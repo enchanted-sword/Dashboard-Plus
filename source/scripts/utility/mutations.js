8bbe77b8ac8e4db37699d14f4adc7cf943b7ee21
enchanted-sword <xlucas4956@gmail.com>
2025-01-19 09:23:27 -0400

    fix root race condition

--- source/scripts/utility/mutations.js
+++ source/scripts/utility/mutations.js
@@ -1,6 +1,6 @@
 import { cellSelector, postSelector } from './document.js';
 
-const root = document.getElementById('root');
+const root = document.getElementById('root') || document.body; // fallback for some early loads
 const addedNodesQueue = [];
 
 let updateQueued = false;
