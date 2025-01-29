2f1ac51cb5bb1fa915129f0203f2819ce941a4b1
enchanted-sword <xlucas4956@gmail.com>
2025-01-29 10:51:15 -0400

    database integration to save on api requests

--- source/scripts/utility/user.js
+++ source/scripts/utility/user.js
@@ -1,4 +1,5 @@
 import { apiFetch } from './tumblr.js';
+import { getIndexedBlogs, updateData } from './database.js';
 
 const fetchedUserInfo = await apiFetch('/v2/user/info').catch((error) => {
   console.error(error);
@@ -41,6 +42,17 @@ export const adminBlogs = userInfo?.blogs?.filter(blog => blog.admin) ?? [];
 export const adminBlogNames = adminBlogs.map(blog => blog.name);
 
 export const isFollowing = async handle => {
-  const { response } = await apiFetch(`/v2/blog/${userInfo.name}/followed_by?query=${handle}`);
-  return response.followedBy;
+  const blog = await getIndexedBlogs(handle);
+
+  if (blog.hasOwnProperty('followedBy')) return blog.followedBy;
+  else {
+    const { response } = await apiFetch(`/v2/blog/${userInfo.name}/followed_by?query=${handle}`);
+
+    if (typeof blog !== 'undefined') {
+      blog.followedBy = response.followedBy;
+      updateData({ blogStore: blog });
+    }
+
+    return response.followedBy;
+  }
 };
