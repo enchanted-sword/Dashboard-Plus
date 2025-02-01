ff7e466bf1a5329fc643abac880a78d677fb64f8
enchanted-sword <xlucas4956@gmail.com>
2025-02-01 12:56:34 -0400

    Finally got a community post rec so now we can nuke them from the dash

--- source/scripts/hideInStreamContent.js
+++ source/scripts/hideInStreamContent.js
@@ -1,24 +1,29 @@
 import { getOptions } from './utility/jsTools.js';
 import { style, s } from './utility/style.js';
-import { mutationManager } from './utility/mutations.js';
+import { mutationManager, postFunction } from './utility/mutations.js';
+import { timelineObject } from './utility/reactProps.js';
 
-let blogRecommendations, tagRecommendations, blazedContent, inStreamAds, communities;
 const blogRecSelector = `[data-cell-id*="timelineObject:carousel"] ${s('listTimelineObject')}:has(${s('blogLinkRecommendationWrapper')})`;
 const tagRecSelector = `[data-cell-id*="timelineObject:carousel"] ${s('listTimelineObject')}:has(${s('tagCard')})`;
 const blazedSelector = `${s('listTimelineObject')}:has([href="#managed-icon__badge-blaze"])`;
 const adSelector = `${s('listTimelineObject')}:has(${s('adTimelineObject')},${s('adMeatballMenu')})`;
 const communitiesRecSelector = `[data-cell-id*="timelineObject:carousel"] ${s('listTimelineObject')}:has(${s('communityCard')})`;
 const hiddenClass = 'dbplus-hiddenInStream';
+const checkedClass = 'dbplus-notCommunityRec';
 const styleElement = style(`
   [data-cell-id*="timelineObject:title"]:has(+ [data-cell-id*="timelineObject:carousel"] ${s('listTimelineObject')}.${hiddenClass}),
     .${hiddenClass} { display: none !important; }
 `);
 
 const hideInStreamElements = elements => $(elements).addClass(hiddenClass);
+const hideCommunityPosts = posts => posts.forEach(async post => {
+  const { recommendationReason, community } = await timelineObject(post);
+  if (recommendationReason && community && recommendationReason.icon === 'community' && !community.isMember) post.classList.add(hiddenClass);
+  else post.classList.add(checkedClass);
+})
 
-export const main = async () => {
+const run = ({ blogRecommendations, tagRecommendations, blazedContent, inStreamAds, communities, communityPosts }) => {
   const selectors = [];
-  ({ blogRecommendations, tagRecommendations, blazedContent, inStreamAds, communities } = await getOptions('hideInStreamContent'));
 
   if (blogRecommendations) selectors.push(blogRecSelector);
   if (tagRecommendations) selectors.push(tagRecSelector);
@@ -26,14 +31,26 @@ export const main = async () => {
   if (inStreamAds) selectors.push(adSelector);
   if (communities) selectors.push(communitiesRecSelector);
 
+  if (communityPosts) postFunction.start(hideCommunityPosts, `:not(.${checkedClass})`);
+  else postFunction.stop(hideCommunityPosts);
+
   if (selectors.length) {
     mutationManager.start(selectors.join(','), hideInStreamElements);
-    document.body.append(styleElement);
   }
+};
+
+export const main = async () => {
+  const preferences = await getOptions('hideInStreamContent');
+
+  run(preferences);
+  document.body.append(styleElement);
 };
 
 export const clean = async () => {
   mutationManager.stop(hideInStreamElements);
+  mutationManager.stop(hideCommunityPosts);
   $(`.${hiddenClass}`).removeClass(hiddenClass);
   styleElement.remove();
 };
+
+export const update = async preferences => run(preferences);
