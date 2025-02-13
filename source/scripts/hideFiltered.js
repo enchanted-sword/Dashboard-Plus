5acc351e46b9536cbd5447853be65059504f6b0b
enchanted-sword <xlucas4956@gmail.com>
2025-02-12 20:09:18 -0400

    filtered terms

--- source/scripts/hideFiltered.js
+++ source/scripts/hideFiltered.js
@@ -8,40 +8,53 @@ const hiddenAttribute = 'data-hideFiltered-hidden';
 const styleElement = style(`${s('listTimelineObject')}:has(article[${hiddenAttribute}]) { display: none; }`);
 const postSelector = `[tabindex="-1"][data-id] article:not([${hiddenAttribute}])`;
 
-let filterOptions, blockedStyle;
+let filterOptions, blockedStyle, blockedTerms, blockedTermList;
 const blocked = {};
+
+const reduceToText = (...content) => content.flat().filter(({ type }) => type === 'text').map(({ text }) => text).join(' ').toLowerCase();
 
 const hideFilteredContent = async posts => {
   for (const post of posts) {
     if (filterOptions.content && post.querySelector(s('filteredScreen'))) {
       post.setAttribute(hiddenAttribute, '');
       continue;
-    }
-    if (filterOptions.blocked) {
-      const { blog, rebloggedFromUuid, rebloggedRootUuid, trail, askingName } = await timelineObject(post);
-      let blogIdList = [];
-      if (blockedStyle.parent && blog.uuid) blogIdList.push(blog.uuid);
-      if (blockedStyle.rebloggedFrom && rebloggedFromUuid) blogIdList.push(rebloggedFromUuid);
-      if (blockedStyle.root && rebloggedRootUuid) blogIdList.push(rebloggedRootUuid);
-      if (blockedStyle.trail && trail.length) {
-        const trailBlogNameList = trail.map(trailItem => trailItem.blog?.name || trailItem.broken_blog_name);
-        blogIdList = blogIdList.concat(trailBlogNameList);
-        if (askingName) blogIdList.push(askingName);
+    } else {
+      const { blog, rebloggedFromUuid, rebloggedRootUuid, content, trail, askingName } = await timelineObject(post);
+
+      if (filterOptions.blockedTerms && blockedTerms.length) {
+        const text = reduceToText(content, ...trail);
+        console.log(text);
+
+        if (blockedTermList.some(term => text.includes(term))) {
+          post.setAttribute(hiddenAttribute, '');
+          continue;
+        }
       }
-
-      if (blogIdList.length === 0) continue;
-
-      for (const blogId of blogIdList) {
-        if (typeof blocked[blogId] === 'undefined') {
-          blocked[blogId] = apiFetch(`/v2/blog/${blogId}/info`)
-            .then(({ response: { blog } }) => blog.isBlockedFromPrimary)
-            .catch(() => Promise.resolve(false));
+      if (filterOptions.blocked) {
+        let blogIdList = [];
+        if (blockedStyle.parent && blog.uuid) blogIdList.push(blog.uuid);
+        if (blockedStyle.rebloggedFrom && rebloggedFromUuid) blogIdList.push(rebloggedFromUuid);
+        if (blockedStyle.root && rebloggedRootUuid) blogIdList.push(rebloggedRootUuid);
+        if (blockedStyle.trail && trail.length) {
+          const trailBlogNameList = trail.map(trailItem => trailItem.blog?.name || trailItem.broken_blog_name);
+          blogIdList = blogIdList.concat(trailBlogNameList);
+          if (askingName) blogIdList.push(askingName);
         }
 
-        const isBlocked = await blocked[blogId];
-        if (isBlocked) {
-          post.setAttribute(hiddenAttribute, '');
-          break;
+        if (blogIdList.length === 0) continue;
+
+        for (const blogId of blogIdList) {
+          if (typeof blocked[blogId] === 'undefined') {
+            blocked[blogId] = apiFetch(`/v2/blog/${blogId}/info`)
+              .then(({ response: { blog } }) => blog.isBlockedFromPrimary)
+              .catch(() => Promise.resolve(false));
+          }
+
+          const isBlocked = await blocked[blogId];
+          if (isBlocked) {
+            post.setAttribute(hiddenAttribute, '');
+            break;
+          }
         }
       }
     }
@@ -49,7 +62,9 @@ const hideFilteredContent = async posts => {
 };
 
 export const main = async function () {
-  ({ filterOptions, blockedStyle } = await getOptions('hideFiltered'));
+  ({ filterOptions, blockedStyle, blockedTerms } = await getOptions('hideFiltered'));
+
+  blockedTermList = blockedTerms.toLowerCase().split(',').map(t => t.trim()).filter(t => t);
 
   postFunction.start(hideFilteredContent, postSelector);
   document.body.append(styleElement);
