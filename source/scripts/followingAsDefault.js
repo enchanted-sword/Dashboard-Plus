cb6b37a0feaaa80165d85d30995446a40e6a703b
enchanted-sword <xlucas4956@gmail.com>
2025-02-28 19:14:59 -0400

    race condition

--- source/scripts/followingAsDefault.js
+++ source/scripts/followingAsDefault.js
@@ -1,7 +1,14 @@
 import { navigate } from './utility/tumblr.js';
 
 export const main = async () => {
-  if (window.location.pathname === '/dashboard/stuff_for_you') navigate('/dashboard/following');
+  if (window.location.pathname === '/dashboard/stuff_for_you') {
+    try {
+      navigate('/dashboard/following');
+    } catch (e) {
+      console.warn(e);
+      window.location.href = 'https://www.tumblr.com/dashboard/following';
+    }
+  }
 }
 
 export const clean = async () => void 0;
