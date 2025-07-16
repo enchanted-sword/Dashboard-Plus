ced6d25cf71ef3f5bf6c3d1f2c1874beb0f30a72
enchanted-sword <xlucas4956@gmail.com>
2025-07-16 20:34:35 -0300

    linkToTheme

--- source/scripts/linkToTheme.js
+++ source/scripts/linkToTheme.js
@@ -1,7 +1,36 @@
 import { getOptions } from './utility/jsTools.js';
+import { mutationManager } from './utility/mutations.js';
+import { s } from './utility/style.js';
+
+const customClass = 'dbplus-customTheme';
+const avatarSelector = `${s('avatar')} ${s('blogLink')}:has(${s('avatarImage')}):not(.${customClass})`;
+const headerSelector = `:is(${s('postHeader')},${s('trailHeader')}) :is(${s('blogLink')},${s('permalink')},.dbplus-rebloggedFrom):not(.${customClass},:has(${s('avatarImage')}))`;
+
+const blogRegex = /\/([\w\d-]+)(?:\/(\d+)(?:\/[\w\d-]+)?)?$/;
+
 let linkOptions;
 
+const fixLinks = links => links.forEach(link => {
+  const a = blogRegex.exec(link?.href);
+  if (!a) return;
+  const [_, blog, postId] = a;
+  const newHref = `https://${blog}.tumblr.com` + (postId ? `/post/${postId}` : '');
+  const newLink = link.cloneNode(true); // cloning to remove navigation event listeners
+  newLink.href = newHref;
+  newLink.classList.add(customClass);
+  newLink.oldHref = link.href;
+
+  link.replaceWith(newLink);
+});
+
 export const main = async () => {
-  ({ linkOptions } = await getOptions('linkToTheme'))
+  ({ linkOptions } = await getOptions('linkToTheme'));
+  if (!linkOptions.avatars && !linkOptions.headers) return;
+
+  const selector = (linkOptions.avatars ? avatarSelector : '') + (linkOptions.avatars && linkOptions.headers ? ' , ' : '') + (linkOptions.headers ? headerSelector : '');
+  mutationManager.start(selector, fixLinks);
+  console.log(selector);
 };
-export const clean = async () => { };
+export const clean = async () => {
+  mutationManager.stop(fixLinks);
+}
