ad246b4a9ce141a4033a9fabbcbafa915f084496
enchanted-sword <xlucas4956@gmail.com>
2025-09-13 20:50:31 -0300

    Fix cell selector

--- source/scripts/utility/document.js
+++ source/scripts/utility/document.js
@@ -1,4 +1,4 @@
 export const postSelector = '[tabindex="-1"][data-id] article';
 export const notificationSelector = '[role="tabpanel"] :is([data-css~="notification"],[data-css~="activityItem"])';
-export const cellSelector = '.__cell';
+export const cellSelector = '[data-css~="cell"]';
 export const conversationSelector = '[data-skip-glass-focus-trap][data-css~="conversation"]';
